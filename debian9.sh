6fba4731a651452c95c1544c6cf8f741bd479259
PR Aiman <58582776+praiman99@users.noreply.github.com>
2019-12-09 22:08:10 +0800

    Update debian9.sh

--- debian9.sh
+++ debian9.sh
@@ -50,7 +50,6 @@ cd
 rm -rf ddos-deflate-master.zip
 
 # install squid3
-apt-get update
 apt-get install squid3
 cat > /etc/squid/squid.conf <<-END
 acl localhost src 127.0.0.1/32 ::1
@@ -59,6 +58,7 @@ acl SSL_ports port 443
 acl Safe_ports port 80
 acl Safe_ports port 21
 acl Safe_ports port 443
+acl Safe_ports port 442
 acl Safe_ports port 444
 acl Safe_ports port 70
 acl Safe_ports port 210
@@ -68,24 +68,21 @@ acl Safe_ports port 488
 acl Safe_ports port 591
 acl Safe_ports port 777
 acl CONNECT method CONNECT
-acl SSH dst xxxxxxxxx-xxxxxxxxx/32
+acl SSH dst xxxxxxxxx-xxxxxxxxx/255.255.255.255
 http_access allow SSH
-http_access allow TCP
 http_access allow manager localhost
 http_access deny manager
 http_access allow localhost
 http_access deny all
-http_port 8888
 http_port 3128
-http_port 8080
 http_port 8000
+http_port 8080
 coredump_dir /var/spool/squid3
 refresh_pattern ^ftp: 1440 20% 10080
 refresh_pattern ^gopher: 1440 0% 1440
 refresh_pattern -i (/cgi-bin/|\?) 0 0% 0
 refresh_pattern . 0 20% 4320
-visible_hostname PR Aiman
-END
+visible_hostname PRAiman
 sed -i $MYIP2 /etc/squid/squid.conf;
 
 # install dropbear
