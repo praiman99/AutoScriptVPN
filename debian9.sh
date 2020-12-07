942c25a0cbe7e31c2372d5ca1ac8ebec23f2bdcd
PR Aiman <58582776+praiman99@users.noreply.github.com>
2020-12-07 21:07:10 +0800

    Update debian9.sh

--- debian9.sh
+++ debian9.sh
@@ -64,9 +64,6 @@ acl Safe_ports port 591
 acl Safe_ports port 777
 acl CONNECT method CONNECT
 acl SSH dst xxxxxxxxx-xxxxxxxxx/255.255.255.255
-acl SSH dst 51.79.157.58-51.79.157.58/32
-acl SSH dst 128.199.221.58-128.199.221.58/32
-acl SSH dst 124.217.230.188-124.217.230.188/32
 http_access allow SSH
 http_access allow manager localhost
 http_access deny manager
